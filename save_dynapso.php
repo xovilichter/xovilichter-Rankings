9ea1fc3154c56e53d9767b5ab1283680b32be31a
root <root@vps67145.ovh.net>
2014-05-20 23:49:30 +0200

    Ergebnisart + Google+ Name & Profil URL

--- save_dynapso.php
+++ save_dynapso.php
@@ -13,7 +13,23 @@ preg_match_all('#<td class="right"><span data-sort="(.*)">(.*)</span></td> <td c
 $rankings_rows = array();
 
 foreach($matches[1] as $n => $position){
-	$rankings_rows[] = $position."\t".$matches[3][$n];
+
+	if(preg_match('#<img class="icon" src="http://www.dynapso.de/images/icons/(.*).png" alt="(.*)"#Uis', $matches[2][$n], $universal_search_match)){
+		$type = $universal_search_match[1];		
+	}else{
+		$type = 'organic';
+	}
+	
+	if(preg_match('#<a href="(.*)" title="(.*)" target="_blank"><img class="ico" src="(.*)" alt="(.*)"#Uis', $matches[5][$n], $google_plus_match)){
+		$google_plus = array(
+			'name' => $google_plus_match[4],
+			'short_url' => $google_plus_match[1],
+		);
+	}else{
+		$google_plus = array();
+	}
+
+	$rankings_rows[] = $position."\t".$matches[3][$n]."\t".$type."\t".(!empty($google_plus['name']) ? $google_plus['name'] : '')."\t".(!empty($google_plus['short_url']) ? $google_plus['short_url'] : '');
 }
 
 $save_file = fopen(dirname(__FILE__)."/dynapso/".date('Y-m-d-H-i').'.txt',"w+");

b84a16d5aac5d7fdde6301dc8dd6e6980157e1cc
root <root@vps67145.ovh.net>
2014-05-21 18:56:14 +0200

    Automatic Commit

--- save_dynapso.php
+++ save_dynapso.php
@@ -1,41 +1,31 @@
 <?php
 
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, "http://www.dynapso.de/xovilichter/"); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_USERAGENT, 'Xovilichter.Dynapso/1.0 (+https://github.com/xovilichter/dynapso)');
-curl_setopt($ch, CURLOPT_REFERER, 'https://github.com/xovilichter/dynapso'); 
-$output = curl_exec($ch);
-curl_close($ch);
-
-preg_match_all('#<td class="right"><span data-sort="(.*)">(.*)</span></td> <td class="link"><span data-sort="(.*)">(.*)</span></td> <td class="googlePlus">(.*)</td>#Uis', $output, $matches);
+include('include/xovilichter_dynapso_scraper.php');
 
 $rankings_rows = array();
+$scraper_results = xovilichter_dynapso_scraper();
 
-foreach($matches[1] as $n => $position){
+// columns
+$rankings_rows[] = 'last_update:'.$scraper_results['last_update'];
+$rankings_rows[] = implode("\t", array('position', 'url', 'type', 'google_plus_name', 'google_plus_short_url'));
 
-	if(preg_match('#<img class="icon" src="http://www.dynapso.de/images/icons/(.*).png" alt="(.*)"#Uis', $matches[2][$n], $universal_search_match)){
-		$type = $universal_search_match[1];		
-	}else{
-		$type = 'organic';
-	}
-	
-	if(preg_match('#<a href="(.*)" title="(.*)" target="_blank"><img class="ico" src="(.*)" alt="(.*)"#Uis', $matches[5][$n], $google_plus_match)){
-		$google_plus = array(
-			'name' => $google_plus_match[4],
-			'short_url' => $google_plus_match[1],
-		);
-	}else{
-		$google_plus = array();
-	}
+foreach($scraper_results['search_results'] as $n => $search_result){
 
-	$rankings_rows[] = $position."\t".$matches[3][$n]."\t".$type."\t".(!empty($google_plus['name']) ? $google_plus['name'] : '')."\t".(!empty($google_plus['short_url']) ? $google_plus['short_url'] : '');
+	$row = array();
+	$row[] = $search_result['position'];
+	$row[] = $search_result['url'];
+	$row[] = $search_result['type'];
+	$row[] = $search_result['google_plus']['name'];
+	$row[] = $search_result['google_plus']['short_url'];
+
+	$rankings_rows[] = implode("\t", $row);
+
 }
 
-$save_file = fopen(dirname(__FILE__)."/dynapso/".date('Y-m-d-H-i').'.txt',"w+");
+$save_file = fopen(dirname(__FILE__)."/archive/dynapso/".date('Y-m-d-H-i').'.txt',"w+");
 fwrite($save_file, implode("\n", $rankings_rows));
 fclose($save_file);
 
-$save_file = fopen(dirname(__FILE__)."/dynapso/current.txt","w+");
+$save_file = fopen(dirname(__FILE__)."/archive/dynapso/current.txt","w+");
 fwrite($save_file, implode("\n", $rankings_rows));
 fclose($save_file);
--- save_xovi_rankings.php
+++ save_xovi_rankings.php
@@ -1,35 +1,28 @@
 <?php 
 
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, "http://www.xovi.de/xovilichter/"); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_USERAGENT, 'Xovilichter.Dynapso/1.0 (+https://github.com/xovilichter/xovi)');
-curl_setopt($ch, CURLOPT_REFERER, 'https://github.com/xovilichter/xovi'); 
-$output = curl_exec($ch);
-curl_close($ch);
-
-// den code ein wenig aufrŠumen
-$output = str_replace("\n", '', $output);
-$output = str_replace("\t", '', $output);
-$output = str_replace('itemprop="itemListElement"', ' ', $output);
-$output = str_replace('  ', ' ', $output);
-$output = str_replace('  ', ' ', $output);
-$output = str_replace('  ', ' ', $output);
-$output = str_replace('  ', ' ', $output);
-$output = str_replace(' <', '<', $output);
-
-preg_match_all('#<div class="line"><span class="(.*)">(.*).</span><span class="(.*)"><a href="(.*)" target="_blank">(.*)</a></span><span class="(.*)">(.*)</span><span class="(.*)">(.*)</span><span class="gplus">(.*)</span></div>#Uis', $output, $matches);
+include('include/xovilichter_xovi_scraper.php');
 
 $rankings_rows = array();
+$scraper_results = xovilichter_xovi_scraper();
 
-foreach($matches[2] as $n => $position){	
-	$rankings_rows[] = $position."\t".$matches[4][$n];
+// columns
+$rankings_rows[] = 'last_update:'.$scraper_results['last_update'];
+$rankings_rows[] = implode("\t", array('position', 'url'));
+
+foreach($scraper_results['search_results'] as $n => $search_result){
+
+	$row = array();
+	$row[] = $search_result['position'];
+	$row[] = $search_result['url'];
+
+	$rankings_rows[] = implode("\t", $row);
+
 }
 
-$save_file = fopen(dirname(__FILE__)."/xovi/".date('Y-m-d-H-i').'.txt',"w+");
+$save_file = fopen(dirname(__FILE__)."/archive/xovi/".date('Y-m-d-H-i').'.txt',"w+");
 fwrite($save_file, implode("\n", $rankings_rows));
 fclose($save_file);
 
-$save_file = fopen(dirname(__FILE__)."/xovi/current.txt","w+");
+$save_file = fopen(dirname(__FILE__)."/archive/xovi/current.txt","w+");
 fwrite($save_file, implode("\n", $rankings_rows));
 fclose($save_file);
